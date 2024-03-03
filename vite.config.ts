ce079b4f2f90db577bf8cd9c4e9ef492c2610cd2
Maksim <maksim@MacBook-Pro-Maksim.local>
2024-03-03 14:53:25 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/youtube-home-page/"
+  base: "youtube-home-page"
 })
